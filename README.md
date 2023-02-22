1db0a1ea131df71d2c234d554589a11c49f12582
Karen Collins <fisheskc@googlemail.com>
2023-02-22 19:24:26 +0000

    player1 turn test

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # knots-and-crosses
 
 - A session/game is started - build the grid single array 0-8 corresponding L-R,T-B (DONE)
-- Player 1 selects their token (ex. X / O)
+- Player 1 selects their token (ex. X / O) (DONE)
 - Player 1 takes a valid turn
 - Player 2 takes a valid turn
 - Player1 makes invalid selection (already filled square)
--- index.test.js
+++ index.test.js
@@ -5,9 +5,23 @@ test("that a user can start a new ", () => {
 	expect(knotsAndCrosses.getBoard()).toEqual([]);
 });
 
-test("player one selects their token", () => {
+test("when tokens are set as [X,O], player1's token is X", () => {
 	knotsAndCrosses.startGame();
 	knotsAndCrosses.setPlayers(["X", "O"]);
 
 	expect(knotsAndCrosses.player1Token).toEqual("X");
+});
+
+test("when tokens are set as [X,O], player2's token is O", () => {
+	knotsAndCrosses.startGame();
+	knotsAndCrosses.setPlayers(["X", "O"]);
+
+	expect(knotsAndCrosses.player2Token).toEqual("O");
+});
+test("Player1 selects a first square, gameBoard has player1's token at index 0", () => {
+	knotsAndCrosses.startGame();
+	knotsAndCrosses.setPlayers(["X", "O"]);
+	knotsAndCrosses.player1Turn(0);
+
+	expect(knotsAndCrosses.getBoard()).toEqual(["X","-","-","-","-","-","-","-","-"]);
 });
